4b1aa264259b99a5e2852ea3d7a970ce651e8c34
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2015-04-28 16:26:44 -0600

    Validates Tracker API Token

---
+++ src/main/webapp/scripts/app/account/trackerApiToken/trackerApiToken.controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('nimblecalApp')
+    .controller('TrackerApiTokenController', function ($rootScope, $scope, $http, Principal) {
+
+        Principal.identity().then(function(account) {
+            $scope.acct = account;
+        });
+
+        $scope.trackerApiToken = function () {
+
+            var config = {
+                headers:  {
+                'X-TrackerToken': $scope.token
+            }
+            };
+
+            $http.get('https://www.pivotaltracker.com/services/v5/my/activity', config).
+                success(function(data, status, headers, config) {
+                    $scope.trackerApiSuccess = true;
+                }).
+                error(function(data, status, headers, config) {
+                    $scope.trackerApiError = true;
+                });
+        };
+    });
