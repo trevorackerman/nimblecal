ebc8c905c299f87b06fbc3fb417838c9fe88f1a0
Ackerman, Trevor <tackerman@ziaconsulting.com>
2015-05-01 14:16:02 -0600

    Improve api token spec and fix displaying of alerts

--- src/test/endToEnd/trackerApiTokenSpec.js
+++ src/test/endToEnd/trackerApiTokenSpec.js
@@ -1,5 +1,6 @@
 describe('Tracker API Token Page', function() {
-    beforeEach(function() {
+    it('should instruct user to check api token when it is not valid and indicate when it is valid', function() {
+        browser.driver.manage().window().setSize(1600, 1200);
         browser.driver.manage().window().maximize();
 
         browser.get('#/login');
@@ -11,19 +12,24 @@ describe('Tracker API Token Page', function() {
 
         element(by.linkText('Account')).click();
         element(by.xpath('//a[@href = "#/trackerApiToken"]')).click();
-    });
 
-    it('should instruct user to check api token when it is not valid', function() {
-        element(by.model('token')).sendKeys('probablynotavalidtoken');
-        $('button.btn-primary').click();
+        expect($('div.alert-success').isDisplayed()).toBeFalsy();
+        expect($('div.alert-danger').isDisplayed()).toBeFalsy();
 
-        expect($('div.alert-danger').getText()).toEqual('Failed to validate API Token.\nCheck your Tracker API token.');
-    });
-
-    it('should inform user when api token is valid', function() {
         element(by.model('token')).sendKeys(browser.params.trackerApiToken);
         $('button.btn-primary').click();
+        browser.waitForAngular().then(function () {
+            expect($('div.alert-success').getText()).toEqual('Valid Token.');
+            expect($('div.alert-danger').isDisplayed()).toBeFalsy();
+        });
 
-        expect($('div.alert-success').getText()).toEqual('Valid Token.');
+        element(by.model('token')).sendKeys('probablynotavalidtoken');
+        $('button.btn-primary').click();
+        browser.waitForAngular().then(function () {
+            expect($('div.alert-success').isDisplayed()).toBeFalsy();
+            expect($('div.alert-danger').getText()).toEqual('Failed to validate API Token.\nCheck your Tracker API token.');
+        });
+
+
     });
 });
