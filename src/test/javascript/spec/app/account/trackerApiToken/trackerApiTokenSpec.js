4e3539254af2de74b4e5ff3d3d7d6cd2e5ea6e6b
Ackerman, Trevor <tackerman@ziaconsulting.com>
2015-05-05 13:13:36 -0600

    Store Tracker API Token in cookieStore when it is valid

--- src/test/javascript/spec/app/account/trackerApiToken/trackerApiTokenSpec.js
+++ src/test/javascript/spec/app/account/trackerApiToken/trackerApiTokenSpec.js
@@ -1,27 +1,88 @@
 'use strict';
 
-describe('Tracker API Token Tests ', function () {
+describe('Tracker API Token Controller ', function () {
+    var httpMock, $scope, cookieStore;
 
     beforeEach(module('nimblecalApp'));
 
-    var $scope, q, Principal, Auth;
+    beforeEach(inject(function($rootScope, $controller, $httpBackend, $cookieStore) {
+        cookieStore = $cookieStore;
+        spyOn(cookieStore, 'put');
+        spyOn(cookieStore, 'remove');
+        spyOn(cookieStore, 'get');
+        httpMock = $httpBackend;
+        $scope = $rootScope.$new();
+    }));
 
-    // define the mock Auth service
-    beforeEach(function() {
-        Auth = {
-            updateAccount: function() {}
-        };
+    function otherStuff() {
+        // WTF is all this?....
+        httpMock.expectGET(/api\/account\?cacheBuster=.*/).respond(200, '');
+        httpMock.expectGET('i18n/en/global.json').respond(200, '');
+        httpMock.expectGET('i18n/en/language.json').respond(200, '');
+        httpMock.expectGET('scripts/components/navbar/navbar.html').respond(200, '');
+        httpMock.expectGET('i18n/en/global.json').respond(200, '');
+        httpMock.expectGET('i18n/en/language.json').respond(200, '');
+        httpMock.expectGET('i18n/en/main.json').respond(200, '');
+        httpMock.expectGET('scripts/app/main/main.html').respond(200, '');
+    }
 
-        Principal = {
-            identity: function() {
-                var deferred = q.defer();
-                return deferred.promise;
-            }
-        };
+    describe('when user has not already entered a token', function () {
+        beforeEach(inject(function($controller){
+            $controller('TrackerApiTokenController', {$scope: $scope});
+        }));
+
+        it('calls tracker api to validate token', function() {
+            httpMock.expectGET('https://www.pivotaltracker.com/services/v5/my/activity',
+                function(headers) {
+                    return headers['X-TrackerToken'] == 'goodtoken';
+                })
+                .respond(200, '');
+            otherStuff();
+
+            $scope.token = 'goodtoken';
+            $scope.validateToken();
+            httpMock.flush();
+            expect($scope.trackerApiSuccess).toBeTruthy();
+            expect($scope.trackerApiError).toBeFalsy();
+        });
+
+        it('indicates when the api token is not valid', function() {
+            httpMock.expectGET('https://www.pivotaltracker.com/services/v5/my/activity',
+                function(headers) {
+                    return headers['X-TrackerToken'] == 'bad';
+                })
+                .respond(403, '');
+            otherStuff();
+            $scope.token = 'bad';
+            $scope.validateToken();
+            httpMock.flush();
+            expect($scope.trackerApiError).toBeTruthy();
+            expect($scope.trackerApiSuccess).toBeFalsy();
+        });
+
+        it ('stores tracker api token in session', function () {
+            httpMock.expectGET('https://www.pivotaltracker.com/services/v5/my/activity')
+                .respond(200, '');
+            otherStuff();
+
+            $scope.token = 'myTrackerToken';
+            $scope.validateToken();
+            httpMock.flush();
+            expect(cookieStore.put).toHaveBeenCalledWith('trackerApiToken', 'myTrackerToken');
+            expect(cookieStore.remove).toHaveBeenCalledWith('trackerApiToken');
+        });
     });
 
+    describe('when user has already entered a token', function () {
+        beforeEach(inject(function($controller) {
+            cookieStore.put('trackerApiToken', 'existingToken');
+            $controller('TrackerApiTokenController', {$scope: $scope});
+        }));
 
-    describe('TrackerApiTokenController', function () {
-
-    });
+        it('indicates they may change their token', function () {
+            expect(cookieStore.get).toHaveBeenCalledWith('trackerApiToken');
+            expect(cookieStore.put).not.toHaveBeenCalledWith('trackerApiToken');
+            expect(cookieStore.remove).not.toHaveBeenCalledWith('trackerApiToken');
+        })
+    })
 });
