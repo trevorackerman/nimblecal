78562045bb553b056bdfdf96b59519634ac47d4c
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2015-05-05 13:13:36 -0600

    Store Tracker API Token in cookieStore when it is valid

--- src/main/webapp/scripts/app/account/trackerApiToken/trackerApiToken.controller.js
+++ src/main/webapp/scripts/app/account/trackerApiToken/trackerApiToken.controller.js
@@ -1,27 +1,26 @@
 'use strict';
 
 angular.module('nimblecalApp')
-    .controller('TrackerApiTokenController', function ($rootScope, $scope, $http, Principal) {
+    .controller('TrackerApiTokenController', function ($rootScope, $scope, $http, $cookieStore) {
+        $scope.trackerApiToken = $cookieStore.get('trackerApiToken');
 
-        Principal.identity().then(function(account) {
-            $scope.acct = account;
-        });
-
-        $scope.trackerApiToken = function () {
+        $scope.validateToken = function () {
             $scope.trackerApiSuccess = false;
             $scope.trackerApiError = false;
+            $cookieStore.remove('trackerApiToken');
 
             var config = {
-                headers:  {
-                'X-TrackerToken': $scope.token
-            }
+                headers: {
+                    'X-TrackerToken': $scope.token
+                }
             };
 
             $http.get('https://www.pivotaltracker.com/services/v5/my/activity', config).
-                success(function(data, status, headers, config) {
+                success(function (data, status, headers, config) {
                     $scope.trackerApiSuccess = true;
+                    $cookieStore.put('trackerApiToken', $scope.token);
                 }).
-                error(function(data, status, headers, config) {
+                error(function (data, status, headers, config) {
                     $scope.trackerApiError = true;
                 });
         };
--- src/test/endToEnd/trackerApiTokenSpec.js
+++ src/test/endToEnd/trackerApiTokenSpec.js
@@ -1,5 +1,5 @@
 describe('Tracker API Token Page', function() {
-    it('should instruct user to check api token when it is not valid and indicate when it is valid', function() {
+    beforeEach(function() {
         browser.driver.manage().window().setSize(1600, 1200);
         browser.driver.manage().window().maximize();
 
@@ -9,27 +9,49 @@ describe('Tracker API Token Page', function() {
         element(by.model('password')).sendKeys('admin');
 
         $('button.btn-primary').click();
+        element(by.linkText('Account')).click();
+        element(by.xpath('//a[@href = "#/trackerApiToken"]')).click();
+    });
 
+    // Wanted to have this be an afterEach() function but the screenshot reporter doesn't do its
+    // work until afterEach() has run, which yields screenshots of the home page and logged out.
+    function logout() {
+        element(by.linkText('Account')).click();
+        element(by.xpath('//a[@ng-click = "logout()"]')).click();
+    }
+
+    it('should not indicate that the token can be changed when a valid token has not been entered', function() {
+        expect($('#changeTrackerApiToken').isDisplayed()).toBeFalsy();
+        logout();
+    });
+
+    it('should instruct user to check api token when invalid', function() {
+        element(by.model('token')).sendKeys('probablynotavalidtoken');
+        $('button.btn-primary').click();
+        expect($('div.alert-success').isDisplayed()).toBeFalsy();
+        expect($('div.alert-danger').getText()).toEqual('Failed to validate API Token.\nCheck your Tracker API token.');
+        logout();
+    });
+
+    it('should indicate when token is valid', function() {
+        element(by.model('token')).sendKeys(browser.params.trackerApiToken);
+        $('button.btn-primary').click();
+        expect($('div.alert-success').getText()).toEqual('Valid Token.');
+        expect($('div.alert-danger').isDisplayed()).toBeFalsy();
+        logout();
+    });
+
+    it('should indicate that the token can be changed when a valid one has already been entered', function() {
+        element(by.model('token')).sendKeys(browser.params.trackerApiToken);
+        $('button.btn-primary').click();
+        expect($('div.alert-success').getText()).toEqual('Valid Token.');
+        expect($('div.alert-danger').isDisplayed()).toBeFalsy();
+
+        browser.get('#/');
         element(by.linkText('Account')).click();
         element(by.xpath('//a[@href = "#/trackerApiToken"]')).click();
 
-        expect($('div.alert-success').isDisplayed()).toBeFalsy();
-        expect($('div.alert-danger').isDisplayed()).toBeFalsy();
-
-        element(by.model('token')).sendKeys(browser.params.trackerApiToken);
-        $('button.btn-primary').click();
-        browser.waitForAngular().then(function () {
-            expect($('div.alert-success').getText()).toEqual('Valid Token.');
-            expect($('div.alert-danger').isDisplayed()).toBeFalsy();
-        });
-
-        element(by.model('token')).sendKeys('probablynotavalidtoken');
-        $('button.btn-primary').click();
-        browser.waitForAngular().then(function () {
-            expect($('div.alert-success').isDisplayed()).toBeFalsy();
-            expect($('div.alert-danger').getText()).toEqual('Failed to validate API Token.\nCheck your Tracker API token.');
-        });
-
-
+        expect($('#changeTrackerApiToken').getText()).toEqual('Change Current Tracker API Token');
+        logout();
     });
 });
