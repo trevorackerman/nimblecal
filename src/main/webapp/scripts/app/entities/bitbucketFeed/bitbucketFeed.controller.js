2bd190949f087dc0255007075a5941f56a447214
Ackerman, Trevor <tackerman@ziaconsulting.com>
2015-05-07 15:12:52 -0600

    Added Bitbucket Feed entity

---
+++ src/main/webapp/scripts/app/entities/bitbucketFeed/bitbucketFeed.controller.js
@@ -0,0 +1,62 @@
+'use strict';
+
+angular.module('nimblecalApp')
+    .controller('BitbucketFeedController', function ($scope, BitbucketFeed, Calendar) {
+        $scope.bitbucketFeeds = [];
+        $scope.calendars = Calendar.query();
+        $scope.loadAll = function() {
+            BitbucketFeed.query(function(result) {
+               $scope.bitbucketFeeds = result;
+            });
+        };
+        $scope.loadAll();
+
+        $scope.showUpdate = function (id) {
+            BitbucketFeed.get({id: id}, function(result) {
+                $scope.bitbucketFeed = result;
+                $('#saveBitbucketFeedModal').modal('show');
+            });
+        };
+
+        $scope.save = function () {
+            if ($scope.bitbucketFeed.id != null) {
+                BitbucketFeed.update($scope.bitbucketFeed,
+                    function () {
+                        $scope.refresh();
+                    });
+            } else {
+                BitbucketFeed.save($scope.bitbucketFeed,
+                    function () {
+                        $scope.refresh();
+                    });
+            }
+        };
+
+        $scope.delete = function (id) {
+            BitbucketFeed.get({id: id}, function(result) {
+                $scope.bitbucketFeed = result;
+                $('#deleteBitbucketFeedConfirmation').modal('show');
+            });
+        };
+
+        $scope.confirmDelete = function (id) {
+            BitbucketFeed.delete({id: id},
+                function () {
+                    $scope.loadAll();
+                    $('#deleteBitbucketFeedConfirmation').modal('hide');
+                    $scope.clear();
+                });
+        };
+
+        $scope.refresh = function () {
+            $scope.loadAll();
+            $('#saveBitbucketFeedModal').modal('hide');
+            $scope.clear();
+        };
+
+        $scope.clear = function () {
+            $scope.bitbucketFeed = {site: null, repositoryOwner: null, repositoryName: null, id: null};
+            $scope.editForm.$setPristine();
+            $scope.editForm.$setUntouched();
+        };
+    });
