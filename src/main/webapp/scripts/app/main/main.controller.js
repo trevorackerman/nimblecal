675a44519fce5a1db8264d7be8ad7fcae722aa22
Ackerman, Trevor <tackerman@ziaconsulting.com>
2015-06-12 15:47:51 -0600

    User can now enter Tracker API Token

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
 angular.module('nimblecalApp')
-    .controller('MainController', function ($scope,ProjectFeed, Principal, User) {
-        $scope.eventSources = [];
+    .controller('MainController', function ($scope,ProjectFeed, Principal, User, localStorageService) {
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        $scope.events = [
+            {title: 'All Day Event',start: new Date(y, m, 1)},
+            {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+            {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+            {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+            {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false}
+        ];
+
+        $scope.eventSources = [ $scope.events ];
+        $scope.trackerApiToken = "";
+
         $scope.isMain = true;
 
         $scope.loadAll = function() {
@@ -20,7 +35,12 @@ angular.module('nimblecalApp')
                     .$promise.then(function(user) {
                         $scope.currentUser = user;
                     });
+                $scope.trackerApiToken = localStorageService.get('trackerApiToken');
                 $scope.loadAll();
             }
         });
+
+        $scope.updateTrackerApiToken = function(newToken) {
+            localStorageService.set('trackerApiToken', newToken);
+        }
     });
--- src/test/endToEnd/homePageSpec.js
+++ src/test/endToEnd/homePageSpec.js
@@ -42,7 +42,7 @@ describe('Home Page', function() {
     it('shows user dropdown to choose project feed on calendar', function() {
         browser.get('#/login');
 
-        element(by.model('username')).sendKeys('oneproject');
+        element(by.model('username')).sendKeys('multipleprojects');
         element(by.model('password')).sendKeys('user');
 
         $('button.btn-primary').click();
@@ -50,6 +50,26 @@ describe('Home Page', function() {
 
         expect($('div.alert-warning').isDisplayed()).toBeFalsy();
         var ddlist = element.all(by.css('div.btn-group ul.dropdown-menu li'));
-        expect(ddlist.count()).toBe(1);
+        expect(ddlist.count()).toBe(3);
+    });
+
+    it('allows the user to store their Tracker API token in session', function() {
+        browser.get('#/login');
+
+        element(by.model('username')).sendKeys('noprojects');
+        element(by.model('password')).sendKeys('user');
+
+        $('button.btn-primary').click();
+        browser.sleep(10);
+
+        expect($('div.alert-danger').getText()).toContain("You need to enter your Tracker API token");
+        element(by.model('trackerApiToken')).sendKeys('notrealtoken');
+        expect($('div.alert-danger').isDisplayed()).toBeFalsy();
+
+        $('button.btn-default').click();
+        browser.get('#/settings');
+        browser.get('#/');
+        expect($('div.alert-danger').isDisplayed()).toBeFalsy();
+
     });
 });
