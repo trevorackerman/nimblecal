44e2f3e1617a7910a1a0b09dadddc56e840ba6c6
Ackerman, Trevor <tackerman@ziaconsulting.com>
2015-06-20 14:17:50 -0600

    Use firefox for karma tests

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -62,7 +62,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['PhantomJS'],
+        browsers: ['Firefox'],
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
