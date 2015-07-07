366f20d721fdbb0cdd34ea2b69b6fef6d6473313
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2015-07-06 22:41:36 -0600

    No longer need timing information in logging messages

--- src/main/java/com/nimblehammer/service/TrackerService.java
+++ src/main/java/com/nimblehammer/service/TrackerService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 public class TrackerService {
@@ -29,13 +28,7 @@ public class TrackerService {
         }
         restClient.setParameter("occurred_after", dateTimeService.getISO8601BeginningOfMonth());
 
-        logger.warn("fetching tracker activities");
-
-        long before = new Date().getTime();
         TrackerActivity[] activities = restClient.get(TRACKER_URL + "/" + projectId + "/activity", TrackerActivity[].class);
-        long after = new Date().getTime();
-        long elapsed = after - before;
-        logger.warn("fetched " + activities.length + " activities from tracker " + elapsed + " elapsed");
         trackerActivities.addAll(Arrays.asList(activities));
 
         return trackerActivities;
